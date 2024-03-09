df41b30c8e3e456bce8c66d321ba388a7db55f2f
FaizBano <159723889+FaizBano@users.noreply.github.com>
2024-03-09 08:26:45 +0500

    quotation of Albert Einstein

---
+++ quote.js
@@ -0,0 +1,6 @@
+"use strict";
+// quotion of Einstien
+let quote = "A persong who never try never do any thing new";
+let author = "Albert Einstian";
+// print the name and message
+console.log(`${author},once said(${quote}`);
---
+++ quote.ts
@@ -0,0 +1,15 @@
+// quotion of Einstien
+
+let quote:string="A persong who never try never do any thing new";
+let author: string= "Albert Einstian";
+
+// print the name and message
+
+console.log(`${author},once said(${quote}`)
+
+
+;
+
+
+
+
